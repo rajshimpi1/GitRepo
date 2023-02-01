5eef5bf791b1387d3a976cc38b03ace1ea948e31
PH10_65800_Abhishek <er.abhishekkople@gmail.com>
2023-02-02 01:24:57 +0530

    new data1

---
+++ timepasspractice/timepasspractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timepasspractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.Write("sum : " + a + b);// 1020
+            Console.ReadKey();
+        }
+    }
+}
